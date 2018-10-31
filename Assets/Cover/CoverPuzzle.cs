ee2a30e29ef8d7d6b0382f9865d1f694341a91da
Omar sleam <ooomar92@gmail.com>
2018-10-31 15:52:10 +0200

    screw you

--- Assets/Cover/CoverPuzzle.cs
+++ Assets/Cover/CoverPuzzle.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 using Raskulls.Events;
 using Raskulls.Variables;
 //using 
-public class CoverPuzzle : MonoBehaviour {
+public class CoverPuzzle : MonoBehaviour
+{
 
     public GameEvent Level1FinishedEvent;
+    public float throwForce = 5;
 
-   readonly bool[] IsScrowWelded = new bool[4];
+    readonly bool[] IsScrowWelded = new bool[4];
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        for (int i = 0; i < IsScrowWelded.Length; i++)
+        {
+            IsScrowWelded[i] = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     internal void ScrewRemoved(int screwNUm)
     {
-        IsScrowWelded[screwNUm-1] = false;
+        IsScrowWelded[screwNUm - 1] = false;
 
-       if(CheckIsPuzzelFinished())
+        if (CheckIsPuzzelFinished())
         {
             PuzzelFinishedAction();
         }
@@ -33,6 +40,9 @@ public class CoverPuzzle : MonoBehaviour {
 
     private void PuzzelFinishedAction()
     {
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().AddForce(transform.forward * -throwForce);
+
         Level1FinishedEvent.Raise();
     }
 
