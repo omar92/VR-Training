3dc98820187d393f693870bfc263d32e09ec38d6
AUEzzat <amr.m.ezzat@gmail.com>
2018-10-31 14:38:05 +0200

    gear rotation and stacking

---
+++ Assets/Scripts/GearRotation.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearRotation : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rigidBody;
+    [SerializeField]
+    MeshRenderer meshRenderer;
+    [SerializeField]
+    float angularVelocity;
+
+    public Vector3 Position
+    {
+        get
+        {
+            return transform.position;
+        }
+        set
+        {
+            transform.position = value;
+        }
+    }
+
+    public Vector3 AngularVelocity
+    {
+        get
+        {
+            return rigidBody.angularVelocity;
+        }
+        set
+        {
+            rigidBody.angularVelocity = value;
+        }
+    }
+
+    public float Diameter
+    {
+        get
+        {
+            return meshRenderer.bounds.size.x;
+        }
+    }
+
+    private void Update()
+    {
+        if (angularVelocity > 0)
+            rigidBody.angularVelocity = new Vector3(0, 0, angularVelocity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Gear"))
+        {
+            GearRotation otherGear = other.GetComponent<GearRotation>();
+            if (otherGear.AngularVelocity.z > 0)
+            {
+                AngularVelocity = -otherGear.AngularVelocity * (otherGear.Diameter / Diameter);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rigidBody.angularVelocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/GearStack.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GearStack : MonoBehaviour
+{
+    public List<GearPin> pins;
+    public Raskulls.Variables.FloatVariable snappingDis;
+
+    public Rigidbody rb;
+
+    public float snapTime = 0.1f;
+
+    Vector3 currentVelocity;
+    
+    public bool gearSnapping;
+
+    public bool gearHeld;
+
+    Vector3 pinPos;
+
+    public GearRotation gearRotation;
+
+    private void Update()
+    {
+        if (gearHeld)
+        {
+            rb.useGravity = false;
+            gearRotation.enabled = false;
+            gearSnapping = StackGear();
+        }
+
+        else if (gearSnapping)
+        {
+            rb.useGravity = false;
+            gearRotation.enabled = true;
+            transform.position = Vector3.SmoothDamp(transform.position, pinPos, ref currentVelocity, snapTime);
+            if ((transform.position - pinPos).magnitude < 0.01f)
+            {
+                gearSnapping = false;
+                transform.position = pinPos;
+            }
+        }
+    }
+
+    bool StackGear()
+    {
+        GearPin closestPin = ClosestPin();
+        if (closestPin != null)
+        {
+            pinPos = closestPin.Position;
+            print(pinPos);
+            transform.forward = closestPin.transform.up;
+            return true;
+        }
+        return false;
+    }
+
+    GearPin ClosestPin()
+    {
+        float closestDis = (pins[0].Position - transform.position).magnitude;
+        GearPin closestPin = pins[0];
+        for (int i = 1; i < pins.Count; i++)
+        {
+            float newDis = (pins[i].Position - transform.position).magnitude;
+
+            if (newDis < closestDis)
+            {
+                closestDis = newDis;
+                closestPin = pins[i];
+            }
+        }
+        if (closestDis < snappingDis.Value)
+            return closestPin;
+        return null;
+    }
+}
