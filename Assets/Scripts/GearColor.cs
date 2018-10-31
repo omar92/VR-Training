cab71d8bb226c0cc4430b894d0a3bd84e8466115
AUEzzat <amr.m.ezzat@gmail.com>
2018-10-31 23:38:07 +0200

    gears puzzle

---
+++ Assets/Scripts/GearColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearColor : MonoBehaviour
+{
+
+    [SerializeField]
+    MeshRenderer meshRenderer;
+
+    [SerializeField]
+    Material movingMaterial;
+
+    [SerializeField]
+    Material stoppedMaterial;
+
+    [SerializeField]
+    GearRotation gearRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gearRotation.AngularVelocity.magnitude > 0)
+        {
+            meshRenderer.material = movingMaterial;
+        }
+        else
+        {
+            meshRenderer.material = stoppedMaterial;
+        }
+    }
+}
