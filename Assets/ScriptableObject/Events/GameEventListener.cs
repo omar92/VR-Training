0b249f1a3068b7fff6de85e30109905c852d831d
Mohamed Hamdy <mohamedhamdy231995@gmail.com>
2018-10-31 11:15:19 +0200

    ScriptableObjectScript

---
+++ Assets/ScriptableObject/Events/GameEventListener.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Raskulls.Events
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [Tooltip("Event to register with.")]
+        public GameEvent Event;
+
+        [Tooltip("Response to invoke when Event is raised.")]
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+
+    }
+}
---
+++ Assets/ScriptableObject/Variables/IntegerVariable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Raskulls.Variables
+{
+
+    [CreateAssetMenu(fileName = "NewIntVariable", menuName = "Raskulls/Variables/IntegerVariable")]
+    public class IntegerVariable : ScriptableObject
+    {
+        public int Value;
+    }
+}
