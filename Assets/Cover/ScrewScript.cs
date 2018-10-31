3916364c1ccc360675fbcf075010116ed2cf7576
Omar sleam <ooomar92@gmail.com>
2018-10-31 13:59:51 +0200

    screw scripts

---
+++ Assets/Cover/ScrewScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewScript : MonoBehaviour {
+
+    public int screwNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ScrewRemoved()
+    {
+        var cover = GetComponentInParent<CoverPuzzle>();
+        if (cover)
+        {
+            cover.ScrewRemoved(screwNum);
+        }
+    }
+}
